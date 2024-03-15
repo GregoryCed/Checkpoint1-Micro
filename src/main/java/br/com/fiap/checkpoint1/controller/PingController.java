edd4f95388d4b76d6e4de0a2f1fe5cd8e3a5c8b1
Gregory Cedraz Araujo <83233195+GregoryCed@users.noreply.github.com>
2024-03-15 15:19:28 -0300

    Finalizado

--- src/main/java/br/com/fiap/checkpoint1/controller/PingController.java
+++ src/main/java/br/com/fiap/checkpoint1/controller/PingController.java
@@ -1,5 +1,6 @@
 package br.com.fiap.checkpoint1.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class PingController {
 
-	@GetMapping("/ping")
-	public String ping() {
-		return "Pong";
-	}
+	@Value("${spring.profiles.active:}")
+    private String Profile;
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Pong em: "+Profile;
+    }
 	
 }
